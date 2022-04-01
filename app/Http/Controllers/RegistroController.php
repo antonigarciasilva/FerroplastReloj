3371d6a07cfeb2180afcfa73e80063146e219077
Antoni Garcia Silva <ags13dxags@gmail.com>
2022-04-01 01:40:21 -0500

    Registro con minutos

--- app/Http/Controllers/RegistroController.php
+++ app/Http/Controllers/RegistroController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\registro;
 use App\Empleado;
+use Carbon\Carbon;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,16 +18,52 @@ class RegistroController extends Controller
      */
     public function index($dni)
     {
-
-        $empleado=Empleado::where('Dni',$dni)->first();
+        //sizeof cuantos registros hay en el array
+        $empleado = Empleado::where('Dni', $dni)->first();
         $registros = Registro::select(
-            'registros.id',  
+            'registros.id',
             DB::raw('DATE_FORMAT(registros.created_at, "%d/%m/%Y %r") as datetime'),
-            'eventos.nombre_evento')
-            ->join('eventos', 'eventos.id', '=', 'registros.evento_id')
+            'registros.created_at',
+            'registros.updated_at',
+
+            'eventos.nombre_evento'
+        )
+            ->join('eventos', 'eventos.id', '=', 'registros.evento_id') //Aquí están comparando?
             ->where('registros.empleado_id', $empleado->id)
             ->get();
+
+        $entrada = 0;
+        $salida = 0;
+        $horaextra = 0;
+        $permiso = 0;
+
         
+
+        for ($i = 0; $i < sizeof($registros); $i++) {
+            switch ($registros[$i]->nombre_evento) {
+                case "Entrada":
+                    if (strtotime("08:10:59") < strtotime($registros[$i]->created_at->toTimeString())) {
+                        $start_date = new DateTime($registros[0]->created_at->toDateString().' 08:10:59');
+                        $since_start = $registros[$i]->created_at->diff($start_date);
+    
+                        $entrada += $since_start->i+1 ;                      
+                    }
+                   
+                    break;
+                case "Salida":
+                    $salida++;
+                    break;
+                case "Hora extra":
+                    $horaextra++;
+                    break;
+                case "Permiso":
+                    $permiso++;
+                    break;
+            }
+        }
+        return $entrada;
+
+        return 'Entrada: ' . $entrada . ' Salida: ' . $salida . ' Horaextra: ' . $horaextra . ' Permiso: ' . $permiso;
         return view('registros.registro')->with(compact('empleado', 'registros'));
     }
 
@@ -36,7 +74,6 @@ class RegistroController extends Controller
      */
     public function create()
     {
-        //
     }
 
     /**
